df7c2a89cda7e5faa127de613827011bb1156840
unknown <marik.3000@hotmail.com>
2020-01-09 18:31:32 +0100

    um

---
+++ flightservice/src/main/java/com/sebastiangds/flightservice/HelloServiceImpl.java
@@ -0,0 +1,23 @@
+package com.sebastiangds.flightservice;
+
+public class HelloServiceImpl extends HelloServiceImplBase {
+
+    @Override
+    public void hello(
+            HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+
+        String greeting = new StringBuilder()
+                .append("Hello, ")
+                .append(request.getFirstName())
+                .append(" ")
+                .append(request.getLastName())
+                .toString();
+
+        HelloResponse response = HelloResponse.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
