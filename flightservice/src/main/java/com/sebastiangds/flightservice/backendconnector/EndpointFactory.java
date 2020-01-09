59023be32a994858b0281291c8405acdb1733187
rasmus-bn <rasbalnor@gmail.com>
2020-01-09 18:43:32 +0100

    connect to backend working

--- flightservice/src/main/java/com/sebastiangds/flightservice/backendconnector/EndpointFactory.java
+++ flightservice/src/main/java/com/sebastiangds/flightservice/backendconnector/EndpointFactory.java
@@ -14,7 +14,7 @@ public class EndpointFactory {
 
     private final String URL_PKG_PREFIXES = "org.jboss.ejb.client.naming";
     private final String INITIAL_CONTEXT_FACTORY = "org.jboss.naming.remote.client.InitialContextFactory";
-    private final String PROVIDER_URL = "http-remoting://localhost:8082";
+    private final String PROVIDER_URL = "http-remoting://35.207.154.57:8082";
     private final String LOOKUP_NAME = "ejb:/4/ContractBean!contract.interfaces.BeanInterface";
 
     public BeanInterface getEndpoint() {
