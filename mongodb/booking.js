cc9fffce69cab7ab0269effe7fd14abc69d4156a
Hallur Við Neyst <Hallur20@github.com>
2020-01-11 18:25:30 +0100

    added work

--- mongodb/booking.js
+++ mongodb/booking.js
@@ -1,17 +1,3 @@
-db.createUser({
-    user : "hallur",
-    pwd : "123",
-    roles : [
-        {
-            role: "readWrite",
-            db: "your-database-name"
-        }
-    ]
-})
-
-db.feedback.insert(
-    {name: "karl", age: "20", location: "copenhagen", gender: "male/female/other", feedback: "i am not happy", rating: 1})
-
 db.booking.insert({bookingId: 1, 
     tickets: [{ticketId: 1, 
         foods: [{name: "peanuts", description: "peanuts description"}, {name: "sandwich", description: "sandwich description"}],
